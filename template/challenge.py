14f0e53c4fea1ec2058b32d657b207229d1d20c8
ed <edouard.hinard@laposte.net>
2023-12-11 10:07:42 +0100

    simpler Input() object

--- template/challenge.py
+++ template/challenge.py
@@ -3,65 +3,16 @@
 import dataclasses
 import math
 import re
-import sys
 
 import aoc
 
 args = aoc.argparse()
 
-
 if args.part == 1:
-    for line in aoc.Input(split=, convert=, groupby=):
-        pass
-    for line in aoc.Input().iter():
-        pass
-# solution:
+    for xxx in aoc.Input(split=, convert=, groupby=):
+# solution: 
 
 
 if args.part == 2:
     pass
-# solution:
-
-
-sys.exit(0)
-@dataclasses.dataclass
-class Monkey:
-    value: None = None
-    left: None = None
-    right: None = None
-    op: None = None
-
-
-text = aoc.Input().read()
-
-# Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-# Sensor at x=9, y=16: closest beacon is at x=10, y=16
-for sensorx, sensory, beaconx, beacony in aoc.Input().iter(
-        split=re.compile(r'Sensor at x=([-]?\d+), y=([-]?\d+): closest beacon is at x=([-]?\d+), y=([-]?\d+)'),
-        convert=int):
-    pass
-
-# 498,4 -> 498,6 -> 496,6
-# 503,4 -> 502,4 -> 502,9 -> 494,9
-paths = aoc.Input().list(split=' -> ', convert=lambda c: tuple(map(int, c.split(','))))
-
-# A
-# B
-# C
-# A
-# B
-# C
-for a, b, c in aoc.Input().iter(groupby=3):
-    pass
-
-# 123
-# 456
-# 798
-#
-# 123
-#
-# 123
-# 456
-for paragraph in aoc.Input().iter(split='', convert=int, groupby='paragraph'):
-    for n1, n2, n3 in paragraph:
-        pass
+# solution: 
