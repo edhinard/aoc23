883ec10553b2a02521e8c31df01f8bf3e9f54db4
ed <edouard.hinard@laposte.net>
2023-12-11 17:49:24 +0100

    04**

--- 04-Scratchcards/Scratchcards.py
+++ 04-Scratchcards/Scratchcards.py
@@ -1,37 +1,31 @@
 #! /usr/bin/env python3
 
-import re
-
 import aoc
 
-args = aoc.argparse()
+args = aoc.argparse(winningslen=10)
 
 
 if args.part == 1:
     sum = 0
-    for line in aoc.Input().iter():
-        numbers = re.findall(r'(\d+)', line)
-        win = set(numbers[1:11])
-        got = set(numbers[11:])
+    for line in aoc.Input(split=None):
+        win = set(line[2:2 + args.winningslen])
+        got = set(line[3 + args.winningslen:])
         nummatches = len(win & got)
         if nummatches:
             sum += 2 ** (nummatches - 1)
     print(sum)
-
 # solution: 21558
 
 
 if args.part == 2:
-    lines = aoc.Input().list()
+    lines = list(aoc.Input(split=None))
     cards = [1] * len(lines)
     for currentcard, copies in enumerate(cards):
         line = lines[currentcard]
-        numbers = re.findall(r'(\d+)', line)
-        win = set(numbers[1:11])
-        got = set(numbers[11:])
-        nummatches = len(win & got)        
+        win = set(line[2:2 + args.winningslen])
+        got = set(line[3 + args.winningslen:])
+        nummatches = len(win & got)
         for i in range(nummatches):
             cards[currentcard + i + 1] += copies
     print(sum(cards))
-
 # solution: 10425665
