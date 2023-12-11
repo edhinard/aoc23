84dd82b3125135795ddf654190356725527e8491
ed <edouard.hinard@laposte.net>
2023-12-11 14:57:09 +0100

    10**

--- 10-PipeMaze/PipeMaze.py
+++ 10-PipeMaze/PipeMaze.py
@@ -5,8 +5,8 @@ import aoc
 args = aoc.argparse()
 
 
-if args.part == 1:
-    map = [line for line in aoc.Input().iter()]
+def cleanup(map):
+    # find the Starting point
     for y, line in enumerate(map):
         for x, pipe in enumerate(line):
             if pipe == 'S':
@@ -15,112 +15,83 @@ if args.part == 1:
         else:
             continue
         break
-    # cheat: S is '-', look at the input
-    count = 1
-    X = start[0] + 1
-    Y = start[1]
-    dir = 'WE'
-    while (X, Y) != start:
-        match dir, pipe:
-            case 'WE', '-':
-                X += 1
-            case 'WE', '7':
-                Y += 1
-                dir = 'NS'
-            case 'WE', 'J':
-                Y -= 1
-                dir = 'SN'
 
-            case 'EW', '-':
-                X -= 1
-            case 'EW', 'F':
-                Y += 1
-                dir = 'NS'
-            case 'EW', 'L':
-                Y -= 1
-                dir = 'SN'
+    # walk the path from start until the loop is closed
+    #  try the 4 possible directions
+    for dX, dY, initialdir in ((1, 0, 'WE'), (0, 1, 'NS'), (-1, 0, 'EW'), (0, -1, 'SN')):
+        looplen = 1
+        loop = [[' '] * len(map[0]) for _ in range(len(map))]
+        X, Y = start
+        X += dX
+        Y += dY
+        dir = initialdir
+        loop[Y][X] = pipe = map[Y][X]
+        while (X, Y) != start:
+            if X < 0 or X >= len(loop[0]) or Y < 0 or Y >= len(loop):
+                # this is not a loop, wrong initial direction
+                break
+            match dir, pipe:
+                case 'WE', '-':
+                    X += 1
+                case 'WE', '7':
+                    Y += 1
+                    dir = 'NS'
+                case 'WE', 'J':
+                    Y -= 1
+                    dir = 'SN'
 
-            case 'NS', '|':
-                Y += 1
-            case 'NS', 'J':
-                X -= 1
-                dir = 'EW'
-            case 'NS', 'L':
-                X += 1
-                dir = 'WE'
+                case 'EW', '-':
+                    X -= 1
+                case 'EW', 'F':
+                    Y += 1
+                    dir = 'NS'
+                case 'EW', 'L':
+                    Y -= 1
+                    dir = 'SN'
 
-            case 'SN', '|':
-                Y -= 1
-            case 'SN', '7':
-                X -= 1
-                dir = 'EW'
-            case 'SN', 'F':
-                X += 1
-                dir = 'WE'
+                case 'NS', '|':
+                    Y += 1
+                case 'NS', 'J':
+                    X -= 1
+                    dir = 'EW'
+                case 'NS', 'L':
+                    X += 1
+                    dir = 'WE'
 
-        count += 1
-        pipe = map[Y][X]
-                
-    print(count // 2)
+                case 'SN', '|':
+                    Y -= 1
+                case 'SN', '7':
+                    X -= 1
+                    dir = 'EW'
+                case 'SN', 'F':
+                    X += 1
+                    dir = 'WE'
+
+                case _:
+                    break
+
+            if loop[Y][X] != ' ':
+                break
+            loop[Y][X] = pipe = map[Y][X]
+            looplen += 1
+
+        if (X, Y) == start:
+            # loop is closed replace pipe at Starting point
+            S = dict(WEWE='-', WENS='L', WESN='F', SNSN='|', NSWE='7', EWNS='J')[f'{initialdir}{dir}']
+            loop[Y][X] = S
+            return loop, looplen
+
+
+map = [line for line in aoc.Input()]
+loop, looplen = cleanup(map)
+
+
+if args.part == 1:
+    print(looplen // 2)
 # solution: 6882
 
 
 if args.part == 2:
-    map = [line for line in aoc.Input().iter()]
-    for y, line in enumerate(map):
-        for x, pipe in enumerate(line):
-            if pipe == 'S':
-                start = (x, y)
-                break
-        else:
-            continue
-        break
-    X, Y  = start
-    loop = [[' '] * len(map[0]) for _ in range(len(map))]
-    loop[Y][X] = '-'
-    dir = 'WE'
-    X += 1
-    while (X, Y) != start:
-        match dir, pipe:
-            case 'WE', '-':
-                X += 1
-            case 'WE', '7':
-                Y += 1
-                dir = 'NS'
-            case 'WE', 'J':
-                Y -= 1
-                dir = 'SN'
-
-            case 'EW', '-':
-                X -= 1
-            case 'EW', 'F':
-                Y += 1
-                dir = 'NS'
-            case 'EW', 'L':
-                Y -= 1
-                dir = 'SN'
-
-            case 'NS', '|':
-                Y += 1
-            case 'NS', 'J':
-                X -= 1
-                dir = 'EW'
-            case 'NS', 'L':
-                X += 1
-                dir = 'WE'
-
-            case 'SN', '|':
-                Y -= 1
-            case 'SN', '7':
-                X -= 1
-                dir = 'EW'
-            case 'SN', 'F':
-                X += 1
-                dir = 'WE'
-
-        pipe = map[Y][X]
-        loop[Y][X] = pipe
-
     count = 0
     for line in loop:
         inside = 0
@@ -135,6 +106,4 @@ if args.part == 2:
                 inside = 1 - inside
             previous = pipe
     print(count)
-            
 # solution: 491
-
