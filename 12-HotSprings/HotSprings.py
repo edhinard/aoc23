48d48110a65ae272c2b8783f064c3b50f1624b90
Compagnie <edouard.hinard@laposte.net>
2023-12-13 22:15:09 +0100

    12**

--- 12-HotSprings/HotSprings.py
+++ 12-HotSprings/HotSprings.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python3
 
 import itertools
-import math
-import re
-
 import aoc
 
 args = aoc.argparse()
@@ -37,17 +34,41 @@ if args.part == 1:
         stats = [int(n) for n in stats.split(',')]
         numnewdamaged = sum(stats) - sum((1 for spring in springs if spring == '#'))
         num += sum(1 for onesprings in possiblesprings(springs, numnewdamaged) if damaged(onesprings) == stats)
-        #print("="*50)
-        #print(springs, stats)
-        #for onesprings in possiblesprings(springs, numnewdamaged):
-        #    print(onesprings, damaged(onesprings))
-        #print(sum(1 for onesprings in possiblesprings(springs, numnewdamaged) if damaged(onesprings) == stats))
-        #print()
-    #print()
     print(num)
-# solution: 
+# solution: 7922
 
 
 if args.part == 2:
-    pass
-# solution: 
+    cache = dict()
+
+    def combinations(springs, stats):
+        if not springs and stats:
+            return 0
+        if not stats:
+            if '#' in springs:
+                return 0
+            else:
+                return 1
+
+        if (springs, stats) in cache:
+            return cache[(springs, stats)]
+
+        comb = 0
+        if springs[0] in '.?':
+            comb += combinations(springs[1:], stats)
+
+        if springs[0] in '#?':
+            l = stats[0]
+            if len(springs) >= l and '.' not in springs[:l] and (len(springs) == l or springs[l] != '#'):
+                comb += combinations(springs[l+1:], stats[1:])
+
+        cache[(springs, stats)] = comb
+        return comb
+
+    num = 0
+    for springs, stats in aoc.Input(split=None):
+        stats = tuple([int(n) for n in stats.split(',')] * 5)
+        springs = '?'.join([springs] * 5)
+        num += combinations(springs, stats)
+    print(num)
+# solution: 18093821750095
