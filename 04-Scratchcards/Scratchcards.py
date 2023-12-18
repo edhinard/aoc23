46e037c8a5a09c815d054770d83dbfc816ba0d72
Compagnie <edouard.hinard@laposte.net>
2023-12-18 15:34:52 +0100

    4**

--- 04-Scratchcards/Scratchcards.py
+++ 04-Scratchcards/Scratchcards.py
@@ -1,16 +1,24 @@
 #! /usr/bin/env python3
 
 import aoc
+import dataclasses
 
-args = aoc.argparse(winningslen=10)
+args = aoc.argparse()
 
 
 if args.part == 1:
     sum = 0
-    for line in aoc.Input(split=None):
-        win = set(line[2:2 + args.winningslen])
-        got = set(line[3 + args.winningslen:])
-        nummatches = len(win & got)
+    for line in aoc.Input():
+        # split line at colon
+        # extract card number from the left part
+        # keep the right part (the list of numbers)
+        # split the right part at pipe to get winning and effective numbers
+        card, numbers = line.split(':')
+        num = int(card.split()[1])
+        winning, effective = (set(num.split()) for num in numbers.split('|'))
+
+        # compute length of union of the two sets and the card value which is accumulated
+        nummatches = len(winning & effective)
         if nummatches:
             sum += 2 ** (nummatches - 1)
     print(sum)
@@ -18,14 +26,31 @@ if args.part == 1:
 
 
 if args.part == 2:
-    lines = list(aoc.Input(split=None))
-    cards = [1] * len(lines)
-    for currentcard, copies in enumerate(cards):
-        line = lines[currentcard]
-        win = set(line[2:2 + args.winningslen])
-        got = set(line[3 + args.winningslen:])
-        nummatches = len(win & got)
-        for i in range(nummatches):
-            cards[currentcard + i + 1] += copies
-    print(sum(cards))
+    @dataclasses.dataclass
+    class Card:
+        macthingnumbers: int
+        number: int = 1
+
+    cards = []
+    for line in aoc.Input():
+        # split line at colon
+        # extract card number from the left part
+        # keep the right part (the list of numbers)
+        # split the right part at pipe to get winning and effective numbers
+        card, numbers = line.split(':')
+        num = int(card.split()[1])
+        winning, effective = (set(num.split()) for num in numbers.split('|'))
+
+        # compute length of union of the two sets
+        # store the number of match of each
+        cards.append(Card(len(winning & effective)))
+
+    # update the number of cards:
+    #  every n macthingnumbers of a card increment by one the number of the n next cards
+    for cardindex, card in enumerate(cards):
+        for i in range(card.macthingnumbers):
+            cards[cardindex + i + 1].number += card.number
+
+    # The total number of cards
+    print(sum(card.number for card in cards))
 # solution: 10425665
