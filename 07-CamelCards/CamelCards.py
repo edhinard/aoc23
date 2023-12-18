776a6e764aaf5c27662dbf983809038c9ae1711d
Compagnie <edouard.hinard@laposte.net>
2023-12-18 18:36:02 +0100

    7**

--- 07-CamelCards/CamelCards.py
+++ 07-CamelCards/CamelCards.py
@@ -6,7 +6,15 @@ import aoc
 
 args = aoc.argparse()
 
+# It is asked to sort the hands. For that the sorted() function is used with its key=function argument.
+# Parts 1 and 2 have their own key function. The key function is given a hand (and a unused bid) and returns
+# the sum of
+#  - a combination value: from 6 (Five of a kind) to 0 (High card)
+#  - a hand value (very simplified compared to poker rules) which equivalent to a base-13 encoding
+# the combination value is multiplied by 13**6 (greater than every hand value in order to give precedence on it)
 
+
+# Combinations differs by the number of identical cards
 def combinationvalue(hand):
     kinds = collections.defaultdict(int)
     for card in hand:
@@ -41,8 +49,9 @@ def combinationvalue(hand):
     return 0
 
 
+# No surprise for part 1 when J is a JACK
 def handkeynojokers(item):
-    hand, bid = item
+    hand, unusedbid = item
 
     cardvalue = {card: rank for rank, card in enumerate('AKQJT98765432'[::-1])}
     handvalue = 0
@@ -51,16 +60,16 @@ def handkeynojokers(item):
 
     return combinationvalue(hand) * 13**6 + handvalue
 
-
+# In part 2, J is a Joker
 def handkeywithjokers(item):
-    hand, bid = item
+    hand, unusedbid = item
 
     cardvalue = {card: rank for rank, card in enumerate('AKQT98765432J'[::-1])}
     handvalue = 0
     for card in hand:
         handvalue = 13 * handvalue + cardvalue[card]
 
-    # find best card
+    # find the best card
     nojokers = [card for card in hand if card != 'J']
     if not nojokers:
         bestcard = 'A'
@@ -86,7 +95,11 @@ if args.part == 2:
 # solution: 250665248
 
 
+
+# Sort the hands (one hand per line)
 hands = sorted(list(aoc.Input(split=None)), key=handkey)
+
+# And then sum the product of the bid and the rank
 sum = 0
 for rank, (hand, bid) in enumerate(hands, start=1):
     sum += rank * int(bid)
