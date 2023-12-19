bd9daf80b7213ff81124f3d7271d2b70382b6098
Compagnie <edouard.hinard@laposte.net>
2023-12-19 13:55:59 +0100

    11**

--- 11-CosmicExpansion/CosmicExpansion.py
+++ 11-CosmicExpansion/CosmicExpansion.py
@@ -2,7 +2,23 @@
 
 import aoc
 
-args = aoc.argparse(expansion=1000000)
+args = aoc.argparse()
+
+
+# Build the list of galaxies with their coordinate in sky map (row, column)
+# in the same time mark the empty rows and columns (without any galaxy)
+map = [line for line in aoc.Input()]
+galaxies = []
+rows = ['.'] * len(map)
+columns = ['.'] * len(map[0])
+for row, line in enumerate(map):
+    for column, item in enumerate(line):
+        if item == '#':
+            galaxies.append((row, column))
+            rows[row] = '#'
+            columns[column] = '#'
+rows = ''.join(rows)
+columns = ''.join(columns)
 
 
 if args.part == 1:
@@ -11,37 +27,18 @@ if args.part == 1:
 
 
 if args.part == 2:
-    expansion = args.expansion
+    expansion = 1000000 if not aoc.TEST else 100
 # solution: 678728808158
 
 
-mapr = []
-rowsplits = []
-for r, row in enumerate(aoc.Input()):
-    mapr.append(row)
-    if set(row) == set('.'):
-        rowsplits.append(r)
-mapc = []
-columnsplits = []
-for c, column in enumerate(zip(*mapr)):
-    mapc.append(column)
-    if set(column) == set('.'):
-        columnsplits.append(c)
-
-galaxies = []
-deltac = 0
-for c, column in enumerate(mapc):
-    if c in columnsplits:
-        deltac += 1
-    deltar = 0
-    for r, item in enumerate(column):
-        if r in rowsplits:
-            deltar += 1
-        if item == '#':
-            galaxies.append((c + deltac * (expansion - 1), r + deltar * (expansion - 1)))
-
+# Take the number of empty rows and columns between each galaxy
+# into account for distance computation
 distance = 0
-for i, (cA, rA) in enumerate(galaxies[:-1]):
-    for cB, rB in galaxies[i+1:]:
-        distance += cB - cA + abs(rB - rA)
+for i, A in enumerate(galaxies[:-1]):
+    for B in galaxies[i+1:]:
+        r1, r2 = sorted([A[0], B[0]])
+        c1, c2 = sorted([A[1], B[1]])
+        distance += \
+            r2 - r1 + (expansion - 1) * rows.count('.', r1, r2 + 1) +\
+            c2 - c1 + (expansion - 1) * columns.count('.', c1, c2 + 1)
 print(distance)
