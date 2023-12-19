850cfca9e849bf82df25b26c746c81b5917a70e9
Compagnie <edouard.hinard@laposte.net>
2023-12-19 11:07:50 +0100

    9**

--- 09-MirageMaintenance/MirageMaintenance.py
+++ 09-MirageMaintenance/MirageMaintenance.py
@@ -6,11 +6,15 @@ args = aoc.argparse()
 
 
 def predict(nums):
+    # Build the differences list of input sequence
+    # until all zeros
     diffs = [nums]
     while any(diffs[-1]):
         prev = diffs[-1]
         diff = [b - a for a, b in zip(prev[:-1], prev[1:])]
         diffs.append(diff)
+
+    # The prediction is the sum of last number of all lists
     return sum((diff[-1] for diff in diffs))
 
 
@@ -20,5 +24,6 @@ if args.part == 1:
 
 
 if args.part == 2:
+    # Reverse the list to predict the past
     print(sum((predict(list(reversed(nums))) for nums in aoc.Input(split=None, convert=int))))
 # solution: 1124
