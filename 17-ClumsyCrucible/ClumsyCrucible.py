fe15e46e7663c8ceab05826ec18e83a4c46807f0
Compagnie <edouard.hinard@laposte.net>
2023-12-19 18:07:27 +0100

    17**

--- 17-ClumsyCrucible/ClumsyCrucible.py
+++ 17-ClumsyCrucible/ClumsyCrucible.py
@@ -1,21 +1,20 @@
 #! /usr/bin/env python3
 
-import bisect
-import math
+import heapq
 import aoc
 
 args = aoc.argparse()
 
 if args.part == 1:
-    minlen = 0
+    minlen = 1
     maxlen = 3
-# solution: 638 (exec time 5m50s)
+# solution: 638
 
 
 if args.part == 2:
     minlen = 4
     maxlen = 10
-# solution: 748 (exec time 15m40s)
+# solution: 748
 
 
 DOWN = 0
@@ -26,95 +25,37 @@ LEFT = 3
 map = [line for line in aoc.Input(split='', convert=int)]
 xmax = len(map[0])
 ymax = len(map)
-distancemax = (xmax - 1) + (ymax - 1)
-loss = 0
-stack = [(distancemax, loss, DOWN, 0, 0), (distancemax, loss, RIGHT, 0, 0)]
-seen = {(0,0,'-'): 0, (0,0,'|'): 0}
-minloss = math.inf
-while stack:
-    distance, loss, dir, X, Y = stack.pop(0)
-    #print(len(stack), len(seen), minloss, (distance, loss, dir, X, Y))
-    if X == xmax - 1 and Y == ymax - 1:
-        minloss = min(minloss, loss)
-        continue
-    if dir == RIGHT:
-        if seen.get((X,Y,'-')) < loss:
-            continue
-        states = []
-        for _ in range(maxlen):
-            X += 1
-            if X == xmax:
-                break
-            distance -= 1
-            loss += map[Y][X]
-            if _ < minlen - 1:
-                continue
-            localminloss = seen.setdefault((X, Y, '|'), math.inf)
-            if loss < localminloss and loss + distance < minloss:
-                state1 = (distance, loss, DOWN, X, Y)
-                state2 = (distance, loss, UP, X, Y)
-                i = bisect.bisect(stack, state1)
-                stack.insert(i, state2)
-                stack.insert(i, state1)
-                seen[(X, Y, '|')] = loss
+queue = [(0, DOWN, 0, 0), (0, RIGHT, 0, 0)]
+seen = set()
 
-    elif dir == LEFT:
-        if seen.get((X,Y,'-')) < loss:
-            continue
-        for _ in range(maxlen):
-            X -= 1
-            if X == -1:
-                break
-            distance += 1
-            loss += map[Y][X]
-            if _ < minlen - 1:
-                continue
-            localminloss = seen.setdefault((X, Y, '|'), math.inf)
-            if loss < localminloss and loss + distance < minloss:
-                state1 = (distance, loss, DOWN, X, Y)
-                state2 = (distance, loss, UP, X, Y)
-                i = bisect.bisect(stack, state1)
-                stack.insert(i, state2)
-                stack.insert(i, state1)
-                seen[(X, Y, '|')] = loss
+# Dijkstra's algorithm to find the minimum loss path to destination
+# Nodes of the graph are blocks at which direction changes. Module heapq is used
+# to always process the node with the minimum loss
+# The seen set is there to avoid looping
+# Once the destination block is reached we know for sure it is with the minimum loss
+while queue:
+    loss, direction, x, y = heapq.heappop(queue)
+    if x == xmax - 1 and y == ymax - 1:
+        break
 
-    elif dir == UP:
-        if seen.get((X,Y,'|')) < loss:
-            continue
-        for _ in range(maxlen):
-            Y -= 1
-            if Y == -1:
-                break
-            distance += 1
-            loss += map[Y][X]
-            if _ < minlen - 1:
-                continue
-            localminloss = seen.setdefault((X, Y, '-'), math.inf)
-            if loss < localminloss and loss + distance < minloss:
-                state1 = (distance, loss, RIGHT, X, Y)
-                state2 = (distance, loss, LEFT, X, Y)
-                i = bisect.bisect(stack, state1)
-                stack.insert(i, state2)
-                stack.insert(i, state1)
-                seen[(X, Y, '-')] = loss
+    dx, dy, dir1, dir2 = {
+        DOWN: (0, 1, RIGHT, LEFT),
+        RIGHT: (1, 0, DOWN, UP),
+        UP: (0, -1, RIGHT, LEFT),
+        LEFT: (-1, 0, DOWN, UP)}[direction]
 
-    elif dir == DOWN:
-        if seen.get((X,Y,'|')) < loss:
+    prevx, prevy = x, y
+    for _ in range(maxlen):
+        x += dx
+        y += dy
+        if x < 0 or x >= xmax or y < 0 or y >= ymax:
+            break
+        loss += map[y][x]
+        if _ < minlen - 1:
             continue
-        for _ in range(maxlen):
-            Y += 1
-            if Y == ymax:
-                break
-            distance -= 1
-            loss += map[Y][X]
-            if _ < minlen - 1:
-                continue
-            localminloss = seen.setdefault((X, Y, '-'), math.inf)
-            if loss < localminloss and loss + distance < minloss:
-                state1 = (distance, loss, RIGHT, X, Y)
-                state2 = (distance, loss, LEFT, X, Y)
-                i = bisect.bisect(stack, state1)
-                stack.insert(i, state2)
-                stack.insert(i, state1)
-                seen[(X, Y, '-')] = loss
-print(minloss)
+        if (prevx, prevy, x, y) not in seen:
+            seen.add((prevx, prevy, x, y))
+            heapq.heappush(queue, (loss, dir1, x, y))
+            heapq.heappush(queue, (loss, dir2, x, y))
+
+print(loss)
