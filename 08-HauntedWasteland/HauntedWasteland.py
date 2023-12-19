3d5d4b8b5615ebd96244d46a105b3ebf10cfc6e6
Compagnie <edouard.hinard@laposte.net>
2023-12-19 11:02:15 +0100

    8**

--- 08-HauntedWasteland/HauntedWasteland.py
+++ 08-HauntedWasteland/HauntedWasteland.py
@@ -9,39 +9,43 @@ import aoc
 args = aoc.argparse()
 
 
-if args.part == 1:
-    input = aoc.Input()
-    instructions = itertools.cycle(next(input))
-    next(input)
-    input.split = re.compile(r'(\w+) = \((\w+), (\w+)\)')
-    nodes = {node: dict(L=left, R=right) for node, left, right in input}
+# Infinite iterator of (L)eft / (R)ight instructions
+input = aoc.Input()
+instructions = itertools.cycle(next(input))
 
-    count = 0
+# empty line
+next(input)
+
+# Binary oriented graph where nodes made of (L)eft / (R)ight descendants are sorted by name
+input.split = re.compile(r'(\w+) = \((\w+), (\w+)\)')
+nodes = {node: dict(L=left, R=right) for node, left, right in input}
+
+
+if args.part == 1:
+    # Just walk the graph from AAA to ZZZ according to instructions and count the steps
+    length = 0
     node = 'AAA'
     while node != 'ZZZ':
         instruction = next(instructions)
         node = nodes[node][instruction]
-        count += 1
+        length += 1
 
-    print(count)
+    print(length)
 # solution: 14257
 
 
 if args.part == 2:
-    input = aoc.Input()
-    next(input)
-    next(input)
-    input.split = re.compile(r'(\w+) = \((\w+), (\w+)\)')
-    nodes = {node: dict(L=left, R=right) for node, left, right in input}
-
-    counts = []
+    # Measure length of all graph walks from ..A to ..Z
+    lengths = []
     for node in [n for n in nodes.keys() if n.endswith('A')]:
         instructions = itertools.cycle(next(aoc.Input()))
-        count = 0
+        length = 0
         while not node.endswith('Z'):
             instruction = next(instructions)
             node = nodes[node][instruction]
-            count += 1
-        counts.append(count)
-    print(math.lcm(*counts))
+            length += 1
+        lengths.append(length)
+
+    # Result is the Least Common Multiple of all lengths
+    print(math.lcm(*lengths))
 # solution: 16187743689077
