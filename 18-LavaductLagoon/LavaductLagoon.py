f9a3a7fbbbcde80fc2fcc00c9ab88f18f8d00d69
Compagnie <edouard.hinard@laposte.net>
2023-12-19 14:46:37 +0100

    18**

--- 18-LavaductLagoon/LavaductLagoon.py
+++ 18-LavaductLagoon/LavaductLagoon.py
@@ -9,21 +9,33 @@ if args.part == 1:
 
 
 if args.part == 2:
+    # For this part instructions are encoded in hexa subtring
     instructions = []
-    for x, y, c in aoc.Input(split=None):
+    for _, _, c in aoc.Input(split=None):
         l = int(c[2:-2], base=16)
         d = {'0':'R', '1':'D', '2':'L', '3':'U'}[c[-2]]
         instructions.append((d, l))
 # solution: 147839570293376
 
-
-X = Y = 0
-S = 0
+# The surface delimited by the closed loop is computed by parts
+# when the digger moves from A to B, the surface increases by
+#
+#     OA x OB
+#     -------
+#        2
+#
+# where O is whatever point (origin is OK) and x is the cross product
+# But that is only the surface delimited by the center of the digger. Complete
+# with half digger width all around. Just add the half perimeter and 1. Easy.
+x = y = 0
+doublesurface = 0
+perimeter = 0
 for direction, length in instructions:
-    Ux, Uy = X, Y
+    Ax, Ay = x, y
     dx, dy = {'R': (1, 0), 'L': (-1, 0), 'U': (0, -1), 'D': (0, 1)}[direction]
-    X += dx * length
-    Y += dy * length
-    Vx, Vy = X, Y
-    S += (Ux * Vy) - (Uy * Vx) + length
-print(int(S / 2 + 1))
+    x += dx * length
+    y += dy * length
+    Bx, By = x, y
+    doublesurface += (Ax * By) - (Ay * Bx)
+    perimeter += length
+print((abs(doublesurface) + perimeter) // 2 + 1)
